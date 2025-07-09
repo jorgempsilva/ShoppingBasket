56e382738d2a687a12377880b5759d1bde0d2aa0
João <birrajoao12@gmail.com>
2025-07-09 19:32:23 +0100

    feat: create domain project

---
+++ Domain/Entities/Basket.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Basket
+{
+    public string Id { get; set; } = string.Empty;
+    public string BuyerId { get; set; } = string.Empty;
+    public List<string> Items { get; set; } = [];
+    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset UpdatedDate { get; set; }
+}
