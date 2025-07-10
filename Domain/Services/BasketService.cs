6e7088a51dd6b82193141179d40e676faf06ff70
João <birrajoao12@gmail.com>
2025-07-10 23:37:09 +0100

    feat: add new post endpoint to controller

---
+++ Domain/Services/BasketService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Domain.Services;
+
+public class BasketService (IBasketRepository itemRepository)
+{
+    private readonly IBasketRepository _itemRepository = itemRepository;
+    
+    public void AddItem(int Id, List<Item> Items)
+    {
+        foreach (var item in Items)
+        {
+            var product = new Item(item.Name, item.Price); 
+            _itemRepository.AddItemToBasket(product); 
+        }     
+    }
+}
--- Infrastructure/Repositories/BasketRepository.cs
+++ Infrastructure/Repositories/BasketRepository.cs
@@ -1,5 +1,19 @@
-﻿namespace Infrastructure.Repositories;
+﻿using Domain.Entities;
+using Domain.Interfaces;
 
-public class BasketRepository
+namespace Infrastructure.Repositories;
+
+public class BasketRepository : IBasketRepository  
 {
+    private readonly Dictionary<Guid, Item> _items = [];
+
+    public void AddItemToBasket(Item item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+
+        if (_items.ContainsKey(item.Id))
+            throw new ArgumentException("Item with the same ID already exists.", nameof(item));
+
+        _items.Add(item.Id, item);
+    }
 }
